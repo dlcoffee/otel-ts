7af2f207ae68e0a3a250425717442cd43d7d45b4
daniel tea <daniel.l.tea@gmail.com>
2022-12-13 09:51:52 -0800

    add honeycomb

--- tracing.ts
+++ tracing.ts
@@ -1,12 +1,19 @@
-// Require dependencies
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 import * as opentelemetry from '@opentelemetry/sdk-node'
+import { HoneycombSDK } from '@honeycombio/opentelemetry-node'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api'
 
 // For troubleshooting, set the log level to DiagLogLevel.DEBUG
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG)
 
-const sdk = new opentelemetry.NodeSDK({
+const sdk: opentelemetry.NodeSDK = new HoneycombSDK({
+  apiKey: process.env.HONEYCOMB_API_KEY,
+  serviceName: process.env.OTEL_SERVICE_NAME,
   traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
   instrumentations: [getNodeAutoInstrumentations()],
 })
